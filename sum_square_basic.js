378d7a6ae698637e8da205efade66e457bc20fff
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-21 23:26:06 +0100

    sum_square challenge

--- sum_square_basic.js
+++ sum_square_basic.js
@@ -3,3 +3,11 @@ const sum_square = (numbers) => {
 }
 
 console.log(sum_square([3, -1, 1, 14]));
+
+const sum_squarev2 = (numbers, index = 0, total=0) => {
+    if (index === numbers.length) return total;
+    if (numbers[index] > 0) total += numbers[index] ** 2
+    return sum_squarev2(numbers, index+1, total)
+};
+
+console.log(sum_squarev2([3, -1, 1, 14]));
