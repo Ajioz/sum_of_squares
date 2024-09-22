5d53f5ccdda1463bd9d46d5e8a5abce2934fa3d6
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-22 16:11:34 +0100

    completed 1 of 2

--- sum_square_sunny.py
+++ sum_square_sunny.py
@@ -9,18 +9,6 @@ def takeTestCase(numberOfCases, testCaseList=[], index=0):
     return takeTestCase(numberOfCases, testCaseList + [numbers], index + 1)  # Recursive call to collect more test cases
 
 
-def process_test_cases(testCaseList, index=0, results=[]):
-    # print(f'test case list is: {len(testCaseList)} with {testCaseList}')
-    # Base case: if the index reaches the length of test cases, return results
-    if index == len(testCaseList):
-        return results
-    
-    numbers = testCaseList[index]  # Unpack the current test case
-    
-    results.append(sum_square(numbers))  # Append the result of sum_of_squares
-    
-    return process_test_cases(testCaseList, index + 1, results)  # Recursive call to the next test case
-
 
 def sum_square(numbers, index=0, total=0):
     if(index == len(numbers)):
@@ -31,12 +19,29 @@ def sum_square(numbers, index=0, total=0):
     
     return sum_square(numbers, index+1, total) 
 
+
+
+def calc_print(testCaseList, index=0, results=[]):
+    # print(f'test case list is: {len(testCaseList)} with {testCaseList}')
+    # Base case: if the index reaches the length of test cases, return results
+    if index == len(testCaseList):
+        return results
+    
+    numbers = testCaseList[index]  # Unpack the current test case
+    
+    results.append(sum_square(numbers))  # Append the result of sum_of_squares
+    
+    return calc_print(testCaseList, index + 1, results)  # Recursive call to the next test case
+
+
+
 def main():
     numberOfCases = int(input("How many cases do you want to perform: "))  # Read number of test cases
     test_cases = takeTestCase(numberOfCases)  # Collect all test cases recursively
-    results = process_test_cases(test_cases)  # Process test cases to get the results
-    # print("\n".join(map(str, results)))  # Output all results
-    print(f'result: {results}')
+    results = calc_print(test_cases)  # Process test cases to get the results
+    for item in results:
+        print(item)
+    
 
 if __name__ == "__main__":
     main()  # Execute the main function
