a6451a9a10fc3574fd1cfcd3fcc265ad3457ca60
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-22 21:12:46 +0100

    hennge challenge begins

---
+++ hennge_pt1.py
@@ -0,0 +1,24 @@
+
+
+
+def input_capture(number_of_iteration, index=0, testCaseList=[]):
+    
+    # Establish a base case
+    if(index == number_of_iteration):
+        return testCaseList
+    
+    depth_of_sum = int(input("Enter size of iterable: "))
+    
+
+
+
+
+def main():
+    number_of_iteration = int(input("Enter the number of test cases: "))
+    print(f'{number_of_iteration} entered')
+
+
+
+
+if __name__ == "__main__":
+    main()
