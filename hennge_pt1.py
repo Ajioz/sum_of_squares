2e3cac724b2ba5565091988f0471295b8eb9e221
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-23 00:05:19 +0100

    added comments

--- hennge_pt1.py
+++ hennge_pt1.py
@@ -23,32 +23,42 @@ def sum_square(numCollection, weight, index=0, cumulative=0):
     if(index == len(numCollection)):
         return cumulative
     
+    # check if its not negative value and the index does not exceed the weight 
     if(numCollection[index] > 0 and index <= (weight-1)):
         cumulative += numCollection[index] ** 2
     
-    # print(f'this is weight: {weight}')
+    # Recursive call to the next test case
     return sum_square(numCollection, weight, index+1, cumulative)
     
 
 def process_capture(testCaseList, index=0, result=[]):
     
+    # Establish a base case for recursion termination
     if(index == len(testCaseList)):
         return result
     
 
+    # tuple un-packing to receive list of numbers and it respective weight
     weight, numbers = testCaseList[index]
     print(f'weight is: {weight}, numbers are: {numbers}')
+    
+    # invoke the sun_square helper method
     total = sum_square(numbers, weight)
     result.append(total)
     
+    # Recursive call to the next test case
     return process_capture(testCaseList, index + 1, result)
 
 
 def main():
+    # Begin in the main loop to track the number of test case
     number_of_iteration = int(input("Enter the number of test cases: "))
+    # invoke the input collection while leveraging on the number of iteration
     testCaseList = input_capture(number_of_iteration)
+    # invoke the process capture while leveraging on the testCaseList 
     result = process_capture(testCaseList)
     
+    # output final result
     for item in result:
         print(item)
 
